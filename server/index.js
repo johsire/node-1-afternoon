f3ed95f244309d757bfbee775d088188800274ed
Josiah Maina <josiah5115@gmail.com>
2018-07-04 17:54:49 -0600

    set end-points in index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
+const mc = require(__dirname + '/controllers/messages_messages.js');
 
 const app = express();
 
-const mc = require(__dirname + '/controllers/messages_messages.js');
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+
+
+
+
+
 
 // parse application/json();
 app.use(bodyParser.json());
