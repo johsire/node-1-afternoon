419e4c3e6f38eb9ab9b27401be936203c4ddeee6
Josiah Maina <josiah5115@gmail.com>
2018-07-04 09:15:33 -0600

    messages controller set-up

---
+++ server/controllers/messages_controller.js
@@ -0,0 +1,17 @@
+var messages = [];
+var id = 0;
+
+modeule.exports = {
+  create: (req, res) => {
+
+  },
+  read: (req, res) => {
+    
+  },
+  update: (req, res) => {
+    
+  },
+  delete: (req, res) => {
+    
+  }
+};
