84d387bd49e2e482eb337f416194eb2388250c69
Josiah Maina <josiah5115@gmail.com>
2018-07-04 17:35:58 -0600

    APIs endpoints setup

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,17 +1,35 @@
 var messages = [];
+import Message from '../../public/src/components/ChatWindow/Message/Message';
 var id = 0;
 
 modeule.exports = {
   create: (req, res) => {
-
+    const { text, time } = req.body;
+    messages.push({ id, text, time });
+    id++;
+    res.status(200).send(messages);
   },
+
   read: (req, res) => {
-    
+    res.status(200).send(messages);
   },
   update: (req, res) => {
-    
+    const { text } = req.body;
+    const updateID = req.params.id;
+    const messageIndex = messages.findIndex(message => message.id == updateID);
+    let message[ messageIndex ];
+
+    messages[ messageIndex ] = {
+      id: message.id,
+      text: text || message.text,
+      time: message.time
+    };
+    res.status(200).send(messages);
   },
   delete: (req, res) => {
-    
+    const deleteID = req.params.id;
+    messageIndex = messages.findIndex(message => message.id == deleteID);
+    message.splice(messageIndex, 1);
+    res.status(200).send(messages);
   }
 };
